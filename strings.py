9ff7e7c232ba448b12c574022204fd07722f7b33
Simon Vialaret <simon.vialaret@orange.fr>
2019-07-25 09:55:55 +0200

    traduction de strings.py : ancienne version dans strings_en.py

--- strings.py
+++ strings.py
@@ -1,28 +1,30 @@
-BAD_BET_ERROR = 'Bad bet! Bet must be of the form A*B (A dice of value B) or the word "dudo"'
+# -*- coding: utf-8 -*-
 
-INVALID_DIE_VALUE_ERROR = 'Illegal bet! Die value must be between 1 and 6.'
-NON_PALIFICO_CHANGE_ERROR = 'Illegal bet! During a palifico round, only current or former palificos can change the bet value.'
-INVALID_NON_WILDCARD_QUANTITY = 'Illegal bet! The last bet\'s die value was 1. If the value is changed to another die value, the quantity must be at least twice the last quantity plus one.'
-INVALID_WILDCARD_QUANTITY = 'Illegal bet! If the bet value is changed to 1, the quantity must be at least half the last quantity.'
-INVALID_BET_EXCEPTION = 'Illegal bet! A bet must have either a higher quantity or a higher value than the last bet.'
+BAD_BET_ERROR = "Enchère non valide ! Une enchère doit être donnée sous la forme 'A*B' (A dés de valeur B), ou bien être 'Menteur' ou 'Compte exact'"
 
-INSUFFICIENT_BOTS = 'You need at least 1 bot! You can\'t play alone!'
-INSUFFICIENT_DICE = 'You need at least 1 die each!'
+INVALID_DIE_VALUE_ERROR = 'Enchère non valide ! La valeur du dé doit être entre 1 et 6'
+NON_PALIFICO_CHANGE_ERROR = 'Enchère non valide ! Pendant le palifico, la valeur du dé ne peut être changée.'
+INVALID_NON_WILDCARD_QUANTITY = 'Enchère non valide ! La dernière enchère était en pacos. Si vous souhaitez changer la valeur, la quantité de dés doit être strictement supérieure au double de la quantité précédente.'
+INVALID_WILDCARD_QUANTITY = 'Enchère non valide ! Pour faire une enchère valide en pacos, la quantité doit être supérieure à la moitié de la quantité précédente.'
+INVALID_BET_EXCEPTION = 'Enchère non valide ! Vous devez indiquer une quantité strictement supérieure à la quantité précédente.'
+
+INSUFFICIENT_BOTS = 'Il faut au moins  1 bot ! Ou trouvez-vous des amis.'
+INSUFFICIENT_DICE = 'Il faut au moins un dé par personne.'
 
 DASHES = '-' * 64
 DASHES_NEW_LINE = DASHES + '\n'
 
 def correct_dudo(quantity, value):
-	return 'There are only {0} {1}s. Good dudo!'.format(quantity, value)
+	return "Il n'y a que {0} {1}. Quel menteur !".format(quantity, value)
 
 def incorrect_dudo(quantity, value):
-	return 'There are actually {0} {1}s. Incorrect dudo!'.format(quantity, value)
+	return "Il y a bien {0} {1}s. Ce n'était pas un menteur".format(quantity, value)
 
 def round_title(round_number, is_palifico_round):
 	if is_palifico_round:
-		title = 'Round {0} - PALIFICO ROUND\n'.format(round_number)
+		title = "Tour {0} - C'est le PALIFICO\n".format(round_number)
 	else:
-		title = 'Round {0}\n'.format(round_number)
+		title = 'Tour {0}\n'.format(round_number)
 	return DASHES_NEW_LINE + title + DASHES
 
 def welcome_message(players):
@@ -32,11 +34,11 @@ def welcome_message(players):
 			if players.index(player) == len(players) - 1:
 				continue
 			elif players.index(player) == len(players) - 2:
-				names_string += ' and '
+				names_string += ' et '
 			else:
 				names_string += ', '
 
-		return DASHES_NEW_LINE + 'Welcome to Perudo! {0} are playing.'.format(names_string)
+		return DASHES_NEW_LINE + 'Jouons à Perudo ! Les joueurs {0}.'.format(names_string)
 
 def winner(name):
-	return DASHES_NEW_LINE + '{0} wins!\n'.format(name) + DASHES
+	return DASHES_NEW_LINE + '{0} a gagné !\n'.format(name) + DASHES
